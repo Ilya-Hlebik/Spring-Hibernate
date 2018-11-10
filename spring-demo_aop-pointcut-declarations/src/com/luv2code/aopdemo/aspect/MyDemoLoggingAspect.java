2e0e41772b2ce97e52a181d416254632126d227a
TalkmeProject <felix.2894@mail.ru>
2018-11-10 22:43:20 +0300

    Work with AOP annitation

--- spring-demo_aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo_aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,13 +8,19 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-	
+
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..) )")
-	private void forDaoPackage() {}
+	private void forDaoPackage() {
+	}
 
 	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>>> Executing @Before advice on addAccount()");
 	}
-	
+
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>>> Perfoming Api analytics");
+	}
+
 }
